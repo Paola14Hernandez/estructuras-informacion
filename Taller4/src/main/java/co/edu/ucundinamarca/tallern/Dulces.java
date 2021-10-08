22f907fb2815524c50a39b672acda5dfa6e9dde0
57322 <57322@192.168.0.6>
2021-10-08 14:48:00 -0500

    taller4

---
+++ Taller4/src/main/java/co/edu/ucundinamarca/tallern/Dulces.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.ucundinamarca.tallern;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Paola Hernandez
+ */
+public class Dulces {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> dulces = new ArrayList();
+        dulces.add("Chocolatina");
+        dulces.add("ChocoRamo");
+        dulces.add("Gomitas Trululu");
+        dulces.add("Bom Bom Bum");
+        dulces.add("Nucita");
+        dulces.add("Galletas Oreo");
+        dulces.add("Mini Chips");
+        dulces.add("m&m's");
+        dulces.add("Pinguinos");
+        dulces.add("Choco Break");
+                
+
+    }
+
+}
--- Taller4/src/main/resources/co/edu/ucundinamarca/tallern/README.txt
+++ Taller4/src/main/resources/co/edu/ucundinamarca/tallern/README.txt
@@ -1,1 +1,7 @@
+¿Que son los iteradores?
+Un iterador es un puntero que se utiliza en un algoritmo para poder recorrer los elementos almacenados en un contenedor.
+Esto debido que se necesita realizar operaciones en las cuales se requiere del recorrido de los contenedores.
 
+¿Como podriamos recorrer un coleccion en java sin la ayuda de un iterador?
+Podemos recorrer una coleccion utilizando ForEach la cual nos permite recorrer una lista de una forma mas compacta 
+y reduciendo el codigo. 
