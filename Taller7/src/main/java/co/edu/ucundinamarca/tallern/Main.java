5b0f0f313b081ce8ca46c71e094f9e37d25f10fa
Taller 7 <57322@192.168.0.6>
2021-10-27 12:52:27 -0500

    Taller7

--- Taller7/src/main/java/co/edu/ucundinamarca/tallern/Main.java
+++ Taller7/src/main/java/co/edu/ucundinamarca/tallern/Main.java
@@ -11,9 +11,45 @@ public class Main {
 
     static Logger log = Logger.getLogger(Main.class.getName());
 
-    public static void main(String[] args) {
-        log.debug("Hello World : this is a debug message");
-        log.info("Hello World : this is an info message");
+    public static void pila(){
+        PilaLista pila=new PilaLista();
+        pila.insertar(1);
+        pila.insertar(2);
+        pila.insertar(3);
+        pila.insertar(4);
+        pila.insertar(5);
+        pila.insertar(6);
+        
+        log.info(pila.quitar());
+        log.info(pila.quitar());
+        log.info(pila.quitar());
+        log.info(pila.quitar());
+        log.info(pila.quitar());
+        log.info(pila.quitar());        
     }
+    public static void cola(){
+        ColaLista cola=new ColaLista();
+        
+        cola.insertar(1);
+        cola.insertar(2);
+        cola.insertar(3);
+        cola.insertar(4);
+        cola.insertar(5);
+        cola.insertar(6);
+        
+        log.info(cola.quitar());
+        log.info(cola.quitar());
+        log.info(cola.quitar());
+        log.info(cola.quitar());
+        log.info(cola.quitar());
+        log.info(cola.quitar());
+        
+    }
+    
+    public static void main(String[] args) {
+       // pila();
+        cola();
+    }
+    
 
 }
