c40aec77fe0e5a8a9218da04f3904b82abc58888
Adopcion mascota <57322@192.168.0.6>
2021-10-27 14:55:52 -0500

    Taller6

--- Taller6/src/main/java/AdopcionMascota/Listas.java
+++ Taller6/src/main/java/AdopcionMascota/Listas.java
@@ -51,6 +51,86 @@ public class Listas {
             actual=actual.siguiente;
         }while(actual!=primero);
     } 
-     //metodos buscar
-     
+     //metodo buscar
+     public void BuscarEdad(int edad1){
+         Nodo nodo=new Nodo();
+        nodo=primero;
+        if (primero != null){
+            do{
+                if(nodo.Edad==edad1){
+                    log.info("Datos encontrados: "+ nodo.Edad+" "+nodo.especie+" "+nodo.sexo+" "+nodo.Raza);
+                }
+                nodo=nodo.siguiente;
+            }while(nodo!= primero);
+        }else{
+             log.info("Dato no encontrado");
+        }
+    }
+      public void BuscarEspecie(String especie1){
+         Nodo nodo=new Nodo();
+        nodo=primero;
+        if (primero != null){
+            do{
+                if(nodo.especie == especie1){
+                    log.info("Datos encontrados: "+ nodo.especie+" "+nodo.Edad+" "+nodo.sexo+" "+nodo.Raza);
+                }
+                nodo=nodo.siguiente;
+            }while(nodo!= primero);
+        }else{
+             log.info("Dato no encontrado");
+        }
+    }
+     public void BuscarSexo(String sexo1){
+         Nodo nodo=new Nodo();
+        nodo=primero;
+        if (primero != null){
+            do{
+                if(nodo.sexo == sexo1){
+                    log.info("Datos encontrados: "+ nodo.sexo+" "+nodo.Edad+" "+nodo.especie+" "+nodo.Raza);
+                }
+                nodo=nodo.siguiente;
+            }while(nodo!= primero);
+        }else{
+             log.info("Dato no encontrado");
+        }
+    }
+     public void BuscarRaza(String raza){
+         Nodo nodo=new Nodo();
+        nodo=primero;
+        if (primero != null){
+            do{
+                if(nodo.Raza ==raza ){
+                    log.info("Datos encontrados: "+ nodo.Raza+" "+nodo.Edad+" "+nodo.especie+" "+nodo.sexo);
+                }
+                nodo=nodo.siguiente;
+            }while(nodo!= primero);
+        }else{
+             log.info("Dato no encontrado");
+        }
+    }
+     //metodo eliminar
+     public void eliminar(String Raza, int edad, String sexo, String especie){
+         Nodo actual=new Nodo();
+         Nodo anterior=new Nodo();
+         actual=primero;
+         anterior=ultimo;
+         do{
+             if(actual.Raza==Raza && actual.Edad ==edad && actual.sexo==sexo && actual.especie==especie){
+                 if(actual==primero){
+                     primero=primero.siguiente;
+                     ultimo.siguiente=primero;
+                     primero.anterior=ultimo;
+                 }else if(actual == ultimo){
+                     ultimo=anterior;
+                     primero.anterior=ultimo;
+                     ultimo.siguiente=primero;
+                 }else{
+                     anterior.siguiente=actual.siguiente;
+                     actual.siguiente.anterior=anterior;
+                 }
+             }
+             anterior= actual;
+             actual=actual.siguiente;
+         }while(actual != primero);
+     }
 }
--- Taller6/src/main/java/AdopcionMascota/Main.java
+++ Taller6/src/main/java/AdopcionMascota/Main.java
@@ -16,11 +16,14 @@ public class Main {
         Listas lista = new Listas();
         int opcion = 0;
         do {
-            log.info("1. Ingresar un Nodo");
+            log.info("1. Ingresar datos al nodo");
             log.info("2. Mostrar la lista");
-            log.info("3. Buscar en la lista");
-            log.info("4. Modificar");
-            log.info("5. Salir");
+            log.info("3. Buscar en la lista por edad");
+            log.info("4. Buscar en la lista por la especie");
+            log.info("5. Buscar en la lista por el sexo");
+            log.info("6. Buscar en la lista por la raza");
+            log.info("7. Eliminar Nodo");
+            log.info("8. Salir");
             log.info("Elija una Opcion");
             opcion = teclado.nextInt();
             switch (opcion) {
@@ -40,8 +43,44 @@ public class Main {
                     log.info("Mostrar lista");
                     lista.Mostrar();
                     break;
+                case 3:
+                    log.info("Buscar");
+                    log.info("Ingrese un dato a buscar por la edad:");
+                    int edad1=teclado.nextInt();
+                    lista.BuscarEdad(edad1);
+                    break;
+                case 4:
+                    log.info("Buscar");
+                    log.info("Ingrese un dato a buscar por la especie:");
+                    String especie1=teclado.next();
+                    lista.BuscarEspecie(especie1);
+                    break;
+                case 5:
+                    log.info("Buscar");
+                    log.info("Ingrese un dato a buscar por el sexo:");
+                    String sexo1=teclado.next();
+                    lista.BuscarSexo(sexo1);
+                    break;
+                case 6:    
+                    log.info("Buscar");
+                    log.info("Ingrese un dato a buscar por la raza");
+                    String raza=teclado.next();
+                    lista.BuscarRaza(raza);
+                    break;
+                case 7:    
+                    log.info("Eliminar Nodo");
+                    log.info("Ingrese la raza de la mascota:");
+                    String Raza1 = teclado.next();
+                    log.info("Ingrese la edad:");
+                    int edad2 = teclado.nextInt();
+                    log.info("Ingrese el sexo:");
+                    String sexo2 = teclado.next();
+                    log.info("Ingresa la especie de la mascota:");
+                    String especie2 = teclado.next();
+                    lista.eliminar(Raza1, edad2, sexo2, especie2);
+                   break;
             }    
-        } while (opcion != 5);
+        } while (opcion != 8);
 
     }
 
