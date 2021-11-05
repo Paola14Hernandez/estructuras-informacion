a485b3b966cc0423cf71e4cdd17acbb8bd8784fe
Taller 7 <57322@192.168.0.6>
2021-11-05 14:54:20 -0500

    Taller 7

---
+++ Taller7/src/main/java/co/edu/ucundinamarca/tallern/Cola.java
@@ -0,0 +1,27 @@
+
+package co.edu.ucundinamarca.tallern;
+
+import java.util.Scanner;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author Paola Hernandez
+ */
+public class Cola {
+    static Logger log = Logger.getLogger(Main.class.getName());
+    Nodo primero;
+    Nodo ultimo;
+    String nombre;
+    int cedula;
+    int turno;
+    String nombreLibro;
+    Scanner teclado= new Scanner(System.in);
+    
+    public Cola (){
+        primero=null;
+        ultimo=null;
+    }
+    
+    //metodo ingresar
+}
---
+++ Taller7/src/main/java/co/edu/ucundinamarca/tallern/Nodo.java
@@ -0,0 +1,15 @@
+
+package co.edu.ucundinamarca.tallern;
+
+/**
+ *
+ * @author Paola Hernandez
+ */
+public class Nodo {
+    int turno=1;
+    int cedula;
+    String nombre;
+    String nombreLibro;
+    Nodo siguiente;
+    Nodo anterior;
+}
