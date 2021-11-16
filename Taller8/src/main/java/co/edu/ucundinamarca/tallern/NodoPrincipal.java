e86695ec060e35a77b1fa6c7dd28995cf6bf3b10
Taller 8 <57322@192.168.0.6>
2021-11-16 12:47:29 -0500

    Taller 8

--- Taller8/src/main/java/co/edu/ucundinamarca/tallern/NodoPrincipal.java
+++ Taller8/src/main/java/co/edu/ucundinamarca/tallern/NodoPrincipal.java
@@ -1,18 +1,46 @@
 
 package co.edu.ucundinamarca.tallern;
 
-import java.util.ArrayList;
+import org.apache.log4j.Logger;
 
 /**
  *
  * @author Paola Hernandez
  */
 public class NodoPrincipal {
-    String elemento;
-   ArrayList<NodoPrincipal> hijos;   
+    
+    static Logger log = Logger.getLogger(NodoPrincipal.class.getName());
+    String info;
+    int numHijos;
+    NodoPrincipal hijo[];
+    NodoPrincipal hijosT[];
+    
+    public NodoPrincipal(String dato){
+        this.info=dato;
+        this.numHijos=0;
+    }
+    
+    public void copiarHijos(){
+        hijosT=new NodoPrincipal[numHijos + 1];
+        for(int i =0; i<this.numHijos; i++){
+            hijosT[i]=hijo[i];
+        }
+    }
+    public void aumentarHijos(NodoPrincipal nodo){
+        copiarHijos();
+        hijosT[this.numHijos]=nodo;
+        hijo=hijosT;
+        this.numHijos++;
+    }
 
-    public NodoPrincipal(String elemento) {
-        hijos=new ArrayList<>();
-        this.elemento=elemento;
-    } 
+    public String getDato() {
+        return info;
+    }
+
+    public void setDato(String dato) {
+        info = dato;
+    }
+    public void verNodo(){
+        log.info(" " + info + " ");
+    }         
 }
